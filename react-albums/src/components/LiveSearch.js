59d23737e7d31c172ed09aa7c4e90b57af12d145
YorkAmule <ayrtonmule@hotmail.com>
2020-09-28 16:29:02 -0400

    Updated.

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,28 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
 
+import axios from "axios";
+
+
+
 export default function LiveSearch(props) {
-  const [term, setTerm] = useState("");
+  // const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+  const url = "https://itunes.apple.com/search?term=Beyonce&country=CA&media=music&entity=album&attribute=artistTerm";
+  // const url = "https://jsonplaceholder.typicode.com/users"
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(({data} )=> console.log(data))
+      .catch(err => console.log(err));
+  },[])
+
+
+
 
   return (
     <Fragment>
@@ -13,8 +30,8 @@ export default function LiveSearch(props) {
         <img src="images/brand.png" alt="Brand" />
       </header>
       <main>
-        <SearchBar onSearch={term => setTerm(term)} />
-        <Results results={results} />
+        {/* <SearchBar onSearch={term => setTerm(term)} /> */}
+        {/* <Results results={results} /> */}
       </main>
     </Fragment>
   );
